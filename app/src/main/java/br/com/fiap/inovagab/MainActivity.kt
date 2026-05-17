3467a30e2b47086cb7c0f4d7e162115436cc96c0
Raphael Monte Ferraz <raphaelmferrazz@gmail.com>
2026-05-17 13:52:35 -0300

    criado fluxo operador, com tela de orientações estratégicas, cadastro de ideias e de minhas ideias.

--- app/src/main/java/br/com/fiap/inovagab/MainActivity.kt
+++ app/src/main/java/br/com/fiap/inovagab/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import br.com.fiap.inovagab.navigation.AppNavGraph
 import br.com.fiap.inovagab.ui.theme.InovaGABTheme
 
